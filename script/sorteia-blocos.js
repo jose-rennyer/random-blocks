465b97ad2e946f8a67899a65ed6bd438cfca85f0
jose-rennyer <joselimao2002@gmail.com>
2022-05-21 15:56:01 -0300

    Primeira versao do randomBlocks 1.0

---
+++ script/sorteia-blocos.js
@@ -0,0 +1,76 @@
+console.log("olá")
+const canvas = document.querySelector("canvas");
+const caneta = canvas.getContext("2d");
+const colors = ["red", "blue", "green", "pink"]
+const canvasLargura = Number(canvas.getAttribute("width"));
+const canvasAltura = Number(canvas.getAttribute("height"));
+const btSorteia = document.querySelector("#bt-random")
+let numRed = 0
+let numGreen = 0
+let numBlue = 0
+let numPink = 0
+
+
+
+
+
+criaBloco(0, 0, 600, 400, "#fff")
+
+function criaBorda(eixoX, eixoY, largura, altura, cor){
+    caneta.strokeStyle = cor
+    caneta.strokeRect(eixoX, eixoY, largura, altura)
+}
+function criaBloco(eixoX, eixoY, largura, altura, cor){
+    caneta.fillStyle = cor;
+    caneta.fillRect(eixoX, eixoY, largura, altura);
+}
+
+function blocoCompleto(eixoX, eixoY, largura, altura, cor, corB){
+    criaBloco(eixoX, eixoY, largura, altura, cor);
+    criaBorda(eixoX, eixoY, largura, altura, corB)
+}
+
+function random(numeroLimite){
+
+    let aleatorio = Math.round(Math.random() * numeroLimite)
+
+    if(aleatorio < 0){
+        aleatorio = 1
+    }
+    return aleatorio
+}
+
+function sorteiaBlocos(){
+    numRed = 0;
+    numGreen = 0;
+    numBlue = 0;
+    numPink = 0;
+
+    for(let a = 0; a < canvasAltura; a+= 50){
+        for(let i = 0; i < canvasLargura; i+= 50){
+            let corDaVez = colors[random(colors.length - 1)]
+            switch(corDaVez){
+                case "red":
+                    numRed++
+                break;
+                case "green":
+                    numGreen++
+                break;
+                case "blue":
+                    numBlue++
+                break;
+                case "pink":
+                    numPink++
+                break;
+            }
+            blocoCompleto(i, a, 50, 50, corDaVez, "#000")
+        }
+    }  
+}
+sorteiaBlocos();
+
+btSorteia.addEventListener("click", function(e){
+    e.preventDefault();
+    sorteiaBlocos();
+})
+
---
+++ script/verificacao.js
@@ -0,0 +1,72 @@
+let buttonVerifica = document.querySelector("#bt-verifica");
+
+buttonVerifica.addEventListener("click", function(e){
+    e.preventDefault();
+
+    let form = document.querySelector("#formulario-de-blocos");
+
+    let erros = verificaBlocos(form);
+    console.log(erros)
+    if(erros.length > 0){
+        limpaErros()
+        for(let i = 0; i < erros.length; i++){
+            makeError(erros[i])
+        }
+        return;
+    }
+
+    form.reset();
+    limpaErros();
+    alert("Parabéns você acertou todos os blocos aleatórios")
+    sorteiaBlocos()
+})
+
+function limpaErros(){
+    let spans = document.querySelectorAll("span");
+    spans.forEach((span)=>{
+        span.textContent = ""
+    })
+}
+
+function makeError(erros){
+    switch(erros){
+        case "Red":
+            escreveErros(".error-red", "vermelho Incorreto")
+        break;
+        case "Blue":
+            escreveErros(".error-blue", "Azul Incorreto")
+        break;
+        case "Green":
+            escreveErros(".error-green", "Verde Incorreto")
+        break;
+        case "Pink":
+            escreveErros(".error-pink", "Rosa Incorreto")
+        break;
+    }
+}
+function verificaBlocos(form){
+    let erros = []
+    if(form.vermelho.value != numRed){
+        erros.push("Red")
+    }
+
+    if(form.azul.value != numBlue){
+        erros.push("Blue")
+    }
+
+    if(form.verde.value != numGreen){
+        erros.push("Green")
+    }
+
+    if(form.rosa.value != numPink){
+        erros.push("Pink")
+    }
+
+    return erros
+}
+
+function escreveErros(elemento, msg){
+    let el = document.querySelector(elemento)
+    el.textContent = ""
+    el.textContent = msg
+}
